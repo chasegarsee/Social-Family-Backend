f19bd70efa234b8e4c281d7f1513b7db20147fa3
chasegarsee <chasegarsee@gmail.com>
2020-01-23 10:44:30 -0600

    Authentication middleware. must be logged in to make posts

--- socialfamily-functions/functions/index.js
+++ socialfamily-functions/functions/index.js
@@ -47,13 +47,47 @@ app.get("/posts", (req, res) => {
 
 /* CREATE POST ROUTE */
 
-app.post("/post", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No Token Found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+
+app.post("/post", FBAuth, (req, res) => {
   if (req.method !== "POST") {
     return res.status(400).json({ error: "Method not allowed" });
   }
+
   const newPost = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
   db.collection("posts")
